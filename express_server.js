37e26ecba2162b790d53501c1693479e380143ea
Tra Nguyen <ntptra85@gmail.com>
2023-05-13 17:33:09 -0600

    add generate random string

--- express_server.js
+++ express_server.js
@@ -2,12 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(string_length) {
+  let random_string = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < string_length; i++) {
+    random_string += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  console.log(random_string);
+  
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
